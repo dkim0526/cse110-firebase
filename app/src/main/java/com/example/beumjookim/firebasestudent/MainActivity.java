4a845a7e546a4284eaf5075e33c9df53154d5098
Daniel Kim <bjkim0526@gmail.com>
2016-05-18 11:47:49 -0700

    added so far during step 6

--- app/src/main/java/com/example/beumjookim/firebasestudent/MainActivity.java
+++ app/src/main/java/com/example/beumjookim/firebasestudent/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.beumjookim.firebasestudent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.firebase.client.Firebase;
 
 public class MainActivity extends AppCompatActivity {
+
+    public class Student(){
+
+        private String name;
+
+        public String getName(){
+            return name;
+        }
+        public void setName(String newName){
+            name = newName;
+        }
+        public String getStudentId(){
+            return studentId;
+        }
+        public void setStudentId(String id){
+            studentId = id;
+        }
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Firebase myFirebaseRef = new Firebase("https://danielkim-cse110.firebaseio.com/student");
+        Firebase.setAndroidContext(this);     // other setup code
         setContentView(R.layout.activity_main);
     }
+    public void onClickBtn(View v){
+        Student student = new Student();
+        student.setName(name);
+        student.setStudentId(id); myFirebaseRef.child(student.getStudentId()).setValue(student);
+    }
+
+
 }
